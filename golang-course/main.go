883256cd77b3014f3c4c03a471d432cce46b1174
hambalee <hambalee@outlook.com>
2022-09-22 14:28:33 +0700

    variable

--- golang-course/main.go
+++ golang-course/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+var numberInt, numberInt2 int = 1000, 2000
+var msg string = "Hello"
+
 func main() {
-	fmt.Println("Hello World!")
+	numberFloat := 1.2
+	fmt.Println(numberInt)
+	fmt.Println(numberInt2)
+	fmt.Println(numberFloat)
+	fmt.Println(msg)
+	fmt.Println(numberFloat + float64(numberInt))
+	fmt.Println(msg + " World!")
+	fmt.Println("num = ", numberFloat)
 }
