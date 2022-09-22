e14be2ec0592b0ad665624a554b0c3edea03683b
hambalee <hambalee@outlook.com>
2022-09-22 14:47:43 +0700

    Array

--- golang-course/main.go
+++ golang-course/main.go
@@ -14,4 +14,15 @@ func main() {
 	fmt.Println(numberFloat + float64(numberInt))
 	fmt.Println(msg + " World!")
 	fmt.Println("num = ", numberFloat)
+	fmt.Printf("%v\n", numberFloat)
+
+	// Array
+	var productName [4]string
+	productName[0] = "A"
+	productName[1] = "B"
+	productName[2] = "C"
+	productName[3] = "D"
+	price := [4]float32{5, 6, 7, 8}
+	fmt.Println(productName)
+	fmt.Println(price)
 }
